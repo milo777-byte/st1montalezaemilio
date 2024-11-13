07b9259f865b8c7fe484a7816379e78cc98ca247
emiliomontaleza <emiliomontaleza@gmail.com>
2024-11-13 16:43:40 +0000

    Second

--- src/app/Components/web-view/web-view.component.ts
+++ src/app/Components/web-view/web-view.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { VisualizadorComponent } from '../visualizador/visualizador.component';
 
 @Component({
   selector: 'app-web-view',
   standalone: true,
-  imports: [],
+  imports: [VisualizadorComponent],
   templateUrl: './web-view.component.html',
   styleUrl: './web-view.component.css'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WebViewComponent } from './Components/web-view/web-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WebViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
